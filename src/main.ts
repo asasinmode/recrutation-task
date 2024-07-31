8ea8f3046d2f0b364131c388d2cdcd20f14e3b03
asasinmode <skin9470@gmail.com>
2024-07-31 21:17:59 +0200

    feat: ssg & deploy workflow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue';
+import { ViteSSG } from 'vite-ssg/single-page';
 import App from './App.vue';
 
 import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 import '~/assets/index.css';
 
-createApp(App).mount('#app');
+export const createApp = ViteSSG(App);
